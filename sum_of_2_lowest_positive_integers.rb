0e6790ba7308dffdd32e8c2280a09199a3507297
gracenak <gracenak@gmail.com>
2021-06-30 15:08:39 -0700

    add comment about a new method learned, #inject

--- sum_of_2_lowest_positive_integers.rb
+++ sum_of_2_lowest_positive_integers.rb
@@ -2,4 +2,6 @@ def sum_two_smallest_numbers(numbers)
     #   array = numbers.sort
     #   array[0] + array[1]
       numbers.min(2).inject(:+)
-    end
+    # inject method acts the same as #reduce. 
+    # when called, #inject method will pass EACH element and ACCUMULATE each sequentially
+end
