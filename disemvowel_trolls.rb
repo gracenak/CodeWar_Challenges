12a37e8eda121a1eb3d07379e2a3f7f8aba0f05f
gracenak <gracenak@gmail.com>
2021-06-30 23:34:56 -0700

    create method that sorts words in numerical order using regex

--- disemvowel_trolls.rb
+++ disemvowel_trolls.rb
@@ -35,3 +35,13 @@ def count_consonants(str)
     end
     return consonants
 end
+
+# Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+# Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+# "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+
+def order(words)
+    # sorted = words.split(" ")
+    # sorted.sort_by { |word| word.scan[/\d/]}.join(" ")
+    words.split.sort_by { |word| word[/\d/].first.to_i }.join(" ")
+end
