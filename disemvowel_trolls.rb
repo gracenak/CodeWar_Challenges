cc999eb6a9fdc6ba4f46d56669c7bbd1049c64d6
gracenak <gracenak@gmail.com>
2021-06-30 20:10:31 -0700

    create method to count the number of vowels in given string

--- disemvowel_trolls.rb
+++ disemvowel_trolls.rb
@@ -1,3 +1,23 @@
+str = "hello my name is grace"
+
 def disemvowel(str)
     str.delete("aeiouAEIOU") 
-  end
+end
+
+def count_vowels(str)
+    vowels = ["a", "e", "i", "o", "u"]
+    i = 0
+    str.split("").each do |char|
+        # convert str into an array
+        # return the elements in the array
+        vowels.each do |vow|
+            if char == vow
+                i += 1
+                # if the char matches the vow in the array, we increase the counter
+                # by 1 with each iteration
+            end
+        end
+    end
+    puts i
+    # print out the number of vowels
+end
--- sum_of_2_lowest_positive_integers.rb
+++ sum_of_2_lowest_positive_integers.rb
@@ -1,7 +1,9 @@
 def sum_two_smallest_numbers(numbers)
     #   array = numbers.sort
     #   array[0] + array[1]
-      numbers.min(2).inject(:+)
-    # inject method acts the same as #reduce. 
-    # when called, #inject method will pass EACH element and ACCUMULATE each sequentially
+    numbers.min(2).inject(:+)
 end
+
+
+# inject method acts the same as #reduce. 
+# when called, #inject method will pass EACH element and ACCUMULATE each sequentially
